ae6632f3a94f0e954d533f3c9cc79420050ee9f9
MohammadJavad14 <mrph14@gamil.com>
2022-09-03 20:05:48 +0430

    first

---
+++ utils/strToJSON.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+
+const timeToMillisecond = (time) => {
+  const [hh, mm, ss, ms] = time.split(":");
+  const timestamp =
+    parseInt(hh) * 3600000 +
+    parseInt(mm) * 6000 +
+    parseInt(ss) * 1000 +
+    parseInt(ms);
+
+  return timestamp;
+};
+
+fs.readFile("demo2-fa.srt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  dataArray = data.split("\r\n");
+  // console.log(dataArray);
+
+  const subtitles = [];
+  const startTimes = [];
+  let j = 0;
+
+  for (let i = 0; i < dataArray.length; i++) {
+    const id = parseInt(dataArray[i]);
+    if (id > 0) {
+      subtitles.push({ id });
+      continue;
+    }
+
+    const times = dataArray[i].split(" --> ");
+    if (times.length === 2) {
+      let startTime = times[0].trim().slice(0, -1);
+      let endTime = times[1].trim().slice(0, -1);
+      startTimes.push(startTime);
+      startTime = startTime.replace(",", ":");
+      endTime = endTime.replace(",", ":");
+      startTimestamp = timeToMillisecond(startTime);
+      endTimestamp = timeToMillisecond(endTime);
+      const duration = endTimestamp - startTimestamp;
+      subtitles[j].duration = duration;
+      continue;
+    }
+    const text = dataArray[i];
+    if (typeof text === "string" && text.length > 0) {
+      // if (subtitle[j]?.text) {
+
+      if (!subtitles[j]) {
+        subtitles[j - 1].text = subtitles[j - 1].text + " " + text.trim();
+      } else {
+        subtitles[j].text = text.trim();
+        j++;
+      }
+      // } else {
+      //   subtitle[j].text += text.trim();
+      // }
+    }
+  }
+
+  const newSubtitles = subtitles.slice(4, subtitles.length);
+  const newStartTimes = startTimes.slice(4, startTimes.length);
+
+  fs.writeFile(
+    "startTime-fa.json",
+    JSON.stringify(newStartTimes),
+    { flag: "wx" },
+    function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    }
+  );
+
+  fs.writeFile(
+    "subtitle-fa.json",
+    JSON.stringify(newSubtitles),
+    { flag: "wx" },
+    function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    }
+  );
+});
